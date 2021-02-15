7a64b02bf4cca3a1f288326d7a3150cdc7479a5b
krissiaflor <60763257+krissiaflor@users.noreply.github.com>
2021-02-15 19:08:33 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let pageBG = document.querySelector('body');
+
+let firstColor = document.querySelector('#first-color');
+let secondColor = document.querySelector('#second-color');
+let bgColorProperty = document.querySelector('.bg-color-text');
+
+let colorInputs = document.querySelectorAll('input');
+
+for (const inputs of colorInputs) {
+  inputs.addEventListener('input',  () => {
+    pageBG.style.background = `linear-gradient(120deg,${firstColor.value},${secondColor.value})`;
+    bgColorProperty.innerText = pageBG.getAttribute('style');
+    bgColorProperty.style.opacity = 0.5;
+  });
+}
+
+function storeCurrentBackground() {}
